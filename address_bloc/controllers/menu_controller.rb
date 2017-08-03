230d87c93ec3b481c57424ce6bda0b17d6d895cd
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-08-02 20:10:47 -0400

    module Integrated

--- address_bloc/controllers/menu_controller.rb
+++ address_bloc/controllers/menu_controller.rb
@@ -1,9 +1,10 @@
 require_relative '../models/address_book'
 require 'bloc_record'
-# require_relative 'integrate'
+require_relative 'integrate'
 
 class MenuController < BlocRecord
   attr_reader :address_book
+  include Integrate
 
   def main_menu
     set unless @set_up
@@ -200,29 +201,4 @@ class MenuController < BlocRecord
       end
     # end
   end
-
-    def set
-      begin
-          self.get_table('address_book')
-      rescue
-          self.create_table('address_book', {name: "VARCHAR(30)"})
-      end
-      begin
-          self.get_table('entry')
-      rescue
-          self.create_table('entry',{
-              address_book_id: 'INTEGER',
-              name: 'VARCHAR(30)',
-              phone_number: 'VARCHAR(30)',
-              email: 'VARCHAR(30)'
-          }, [['address_book_id', 'address_book(id)']] )
-      end
-      @set_up = true
-    end
-
-    def drop_tables
-      self.drop_table('entry')
-      self.drop_table('address_book')
-      @set_up = false
-    end
 end
